39f493b4ef7fbe0004e532d5d3ededf50acf805f
dpl <dpl0@users.noreply.github.com>
2022-12-02 21:30:20 +0100

    Added solution to day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    if let Ok(file) = File::open("input") {
+        let lines = BufReader::new(file).lines();
+        let mut elfs: Vec<u32> = vec![];
+        let mut counted_calories: u32 = 0;
+
+        // Sum up calories
+        for l in lines {
+            match l {
+                Ok(line) => {
+                    if line.is_empty() {
+                        elfs.push(counted_calories);
+                        counted_calories = 0;
+                    } else {
+                        let n: u32 = line.parse().unwrap();
+                        counted_calories += n;
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        elfs.sort();
+        let bigger = elfs[elfs.len() - 1];
+        println!("maximum: {:?}", bigger);
+
+        // Lazy and hacky
+        let last_three = bigger + elfs[elfs.len() - 2] + elfs[elfs.len() - 3];
+        println!("sum(last_three): {:?}", last_three);
+    }
+}
