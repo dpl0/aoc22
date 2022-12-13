f5099e1ba94529f3c3342819639334802ab40976
dpl <dpl0@users.noreply.github.com>
2022-12-13 21:26:47 +0100

    Improved my solution for day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs::read_to_string;
+#![feature(iter_next_chunk)]
 
-use regex::Regex;
+use std::fs::read_to_string;
 
 type Crate = char;
 type Stack = Vec<Crate>;
@@ -13,10 +13,10 @@ impl StackMachine {
     pub fn apply_instruction_cratemover_9000(&mut self, instruction: &MoveInstruction) {
         // Start counting from 0
         let origin = instruction.origin - 1;
-        let destination = instruction.destination - 1;
+        let to = instruction.to - 1;
         for _ in 0..instruction.amount {
             if let Some(crate_letter) = self.0[origin].pop() {
-                self.0[destination].push(crate_letter);
+                self.0[to].push(crate_letter);
             }
         }
     }
@@ -25,7 +25,7 @@ impl StackMachine {
     pub fn apply_instruction_cratemover_9001(&mut self, instruction: &MoveInstruction) {
         // Start counting from 0
         let origin = instruction.origin - 1;
-        let destination = instruction.destination - 1;
+        let to = instruction.to - 1;
 
         let mut crate_buffer: Vec<Crate> = vec![];
         for _ in 0..instruction.amount {
@@ -34,32 +34,12 @@ impl StackMachine {
             }
         }
         for letter in crate_buffer {
-            self.0[destination].push(letter);
+            self.0[to].push(letter);
         }
     }
 
-    pub fn get_top_crates(&self) -> Vec<Crate> {
-        let mut retval: Vec<Crate> = vec![];
-        for s in &self.0 {
-            if let Some(c) = s.last() {
-                retval.push(*c);
-            }
-        }
-        retval
-    }
-
-    fn get_stack_number(number_line: &str) -> u32 {
-        //  1   2   3
-        let contains_numbers = Regex::new(r"( /d  ?)*").unwrap();
-        assert!(contains_numbers.is_match(number_line));
-        // Get last number of line
-        let Ok(number_of_stacks) = number_line.split_whitespace()
-            .last()
-            .expect("Couldn't get last field of the numbers line")
-            .parse::<u32>() else {
-            panic!("Couldn't parse last element of stack number");
-        };
-        number_of_stacks
+    pub fn get_top_crates(self) -> String {
+        self.0.iter().filter_map(|stack| stack.last().copied()).collect()
     }
 
     pub fn new(stack_repr: Vec<&str>) -> Self {
@@ -73,25 +53,25 @@ impl StackMachine {
 
         // Handle the line with the numbers
         let Some(number_line) = lines.next() else { panic!("Couldn't get the stack machine's first line");};
-        let number_of_stacks = Self::get_stack_number(number_line);
+        let number_of_stacks = (number_line.len() + 1) / 4;
 
         // Populate the stack machine first with empty vectors
-        let mut stack_machine = StackMachine(vec![]);
-        for _ in 0..number_of_stacks {
-            stack_machine.0.push(vec![]);
-        }
+        let mut stack_machine = StackMachine(vec![vec![]; number_of_stacks]);
 
-        let mut stack_index = 0;
         for line in lines {
-            for (i, c) in line.chars().enumerate().step_by(4) {
-                if c == '[' {
-                    let crate_letter: Crate = line.as_bytes()[i + 1] as Crate;
-                    stack_machine.0[stack_index].push(crate_letter);
+            let mut chars = line.chars();
+            for stack_index in 0..number_of_stacks {
+                let Ok([_, crate_id, _]) = chars.next_chunk() else {
+                    panic!("Badly formatted stack. Check the MoveCrater documentation.");
+                };
+
+                if crate_id != ' ' {
+                    stack_machine.0[stack_index].push(crate_id);
                 }
-                stack_index += 1;
+
+                // Ignore space at the end
+                chars.next();
             }
-            // Start all over
-            stack_index = 0;
         }
         stack_machine
     }
@@ -101,43 +81,25 @@ impl StackMachine {
 pub struct MoveInstruction {
     amount: usize,
     origin: usize,
-    destination: usize,
+    to: usize,
 }
 
 impl MoveInstruction {
     /// Parses the actual instruction from the line
     pub fn new(line: &str) -> Self {
-        // move A from B to C
         let mut splitted = line.split_whitespace();
 
-        // Those 1 are Dark Magic, honestly.
-        // It's because `nth` partially consumes the iterator.
-        let (Some(amount), Some(origin), Some(destination)) = (splitted.nth(1), splitted.nth(1), splitted.nth(1)) else {
-            panic!("Couldn't get field from instruction");
+        // move A from B to C
+        let Ok(["move", amount, "from", origin, "to", to]) = splitted.next_chunk() else {
+            panic!("Badly formatted instruction.");
         };
 
-        let (Ok(amount), Ok(origin), Ok(destination)) = (amount.parse(), origin.parse(), destination.parse()) else {
+        let (Ok(amount), Ok(origin), Ok(to)) = (amount.parse(), origin.parse(), to.parse()) else {
             panic!("Couldn't parse fields from instruction");
         };
 
-        MoveInstruction {
-            amount,
-            origin,
-            destination,
-        }
+        MoveInstruction { amount, origin, to }
     }
-}
-
-pub fn parse_instructions(lines: Vec<&str>) -> Vec<MoveInstruction> {
-    let instruction_regex = Regex::new(r"^move \d+ from \d+ to \d+$").unwrap();
-    lines
-        .iter()
-        .filter(|l| !l.is_empty())
-        .map(|l| {
-            assert!(instruction_regex.is_match(l));
-            MoveInstruction::new(l)
-        })
-        .collect()
 }
 
 fn main() {
@@ -154,12 +116,17 @@ fn main() {
     let mut stack_first_problem: StackMachine = StackMachine::new(lines_stack);
     let mut stack_second_problem: StackMachine = stack_first_problem.clone();
 
-    let instructions: Vec<MoveInstruction> = parse_instructions(lines_instructions);
+    let instructions: Vec<MoveInstruction> = lines_instructions
+        .into_iter()
+        .filter(|l| !l.is_empty())
+        .map(MoveInstruction::new)
+        .collect();
+
     for instruction in instructions {
         stack_first_problem.apply_instruction_cratemover_9000(&instruction);
         stack_second_problem.apply_instruction_cratemover_9001(&instruction);
     }
 
-    println!("Answer for first problem: {:?}", stack_first_problem.get_top_crates());
-    println!("Answer for second problem: {:?}", stack_second_problem.get_top_crates());
+    println!("Answer for first problem: {}", stack_first_problem.get_top_crates());
+    println!("Answer for second problem: {}", stack_second_problem.get_top_crates());
 }
